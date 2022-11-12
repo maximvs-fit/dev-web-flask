dbc653f8fa2b981073f4010abee89517f78a7b4b
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2022-11-12 11:13:45 -0300

    registra blueprint de cursos

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from senha import senha_bp
 from contato import contato_bp
+from cursos import cursos_bp
 
 app = Flask(__name__)
 app.config.from_object('app.config.Configuracao')
 
 app.register_blueprint(senha_bp, url_prefix='/senha')
 app.register_blueprint(contato_bp, url_prefix='/contato')
+app.register_blueprint(cursos_bp, url_prefix='/cursos')
