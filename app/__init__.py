5e40b7e98ee6e98621fcdba598958c1715675512
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2022-11-12 10:54:30 -0300

    registra bp de contato e atualiza template base com bloco de estilos e favicon

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from senha import senha_bp
+from contato import contato_bp
 
 app = Flask(__name__)
 app.config.from_object('app.config.Configuracao')
 
 app.register_blueprint(senha_bp, url_prefix='/senha')
+app.register_blueprint(contato_bp, url_prefix='/contato')
